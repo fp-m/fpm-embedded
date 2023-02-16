e500b652e9bef15f572fe33bb92c64d9994ea1db
Serge Vakulenko <serge.vakulenko@gmail.com>
2023-02-15 22:58:24 -0800

    Add commands for filesystem.

--- include/rpm/internal.h
+++ include/rpm/internal.h
@@ -15,10 +15,25 @@ extern jmp_buf rpm_saved_point; // TODO: move to the system area
 //
 // Shell commands.
 //
+void rpm_cmd_cat(int argc, char *argv[]);
+void rpm_cmd_cd(int argc, char *argv[]);
 void rpm_cmd_clear(int argc, char *argv[]);
+void rpm_cmd_copy(int argc, char *argv[]);
 void rpm_cmd_date(int argc, char *argv[]);
+void rpm_cmd_dir(int argc, char *argv[]);
 void rpm_cmd_echo(int argc, char *argv[]);
+void rpm_cmd_eject(int argc, char *argv[]);
 void rpm_cmd_help(int argc, char *argv[]);
+void rpm_cmd_mkdir(int argc, char *argv[]);
+void rpm_cmd_more(int argc, char *argv[]);
+void rpm_cmd_mount(int argc, char *argv[]);
+void rpm_cmd_popd(int argc, char *argv[]);
+void rpm_cmd_pushd(int argc, char *argv[]);
+void rpm_cmd_pwd(int argc, char *argv[]);
 void rpm_cmd_reboot(int argc, char *argv[]);
+void rpm_cmd_remove(int argc, char *argv[]);
+void rpm_cmd_rename(int argc, char *argv[]);
+void rpm_cmd_rmdir(int argc, char *argv[]);
 void rpm_cmd_time(int argc, char *argv[]);
 void rpm_cmd_ver(int argc, char *argv[]);
+void rpm_cmd_vol(int argc, char *argv[]);
--- kernel/cmd/cmd_help.c
+++ kernel/cmd/cmd_help.c
@@ -28,13 +28,28 @@ void rpm_cmd_help(int argc, char *argv[])
     }
 
     rpm_puts("RP/M built-in commands are:\r\n");
+    rpm_puts("CAT or TYPE     Display the contents of a text file\r\n");
+    rpm_puts("CD              Change current directory\r\n");
     rpm_puts("CLEAR or CLS    Clear the console screen\r\n");
+    rpm_puts("CP or COPY      Copy files or directories\r\n");
     rpm_puts("DATE            Show or change the system date\r\n");
     rpm_puts("ECHO            Copy text directly to the console output\r\n");
+    rpm_puts("EJECT           Release removable disk device\r\n");
     rpm_puts("HELP or ?       Show all built-in commands\r\n");
+    rpm_puts("LS or DIR       List the contents of a directory\r\n");
+    rpm_puts("MKDIR           Create a directory\r\n");
+    rpm_puts("MORE            Display output in pages\r\n");
+    rpm_puts("MOUNT           Engage removable disk device\r\n");
+    rpm_puts("MV or RENAME    Rename a file\r\n");
+    rpm_puts("POPD            Restore the directory to the last one saved with PUSHD\r\n");
+    rpm_puts("PUSHD           Change to a new directory, saving the current one\r\n");
+    rpm_puts("PWD             Show path of current working directory\r\n");
     rpm_puts("REBOOT          Restart the RP/M kernel\r\n");
+    rpm_puts("RM or ERASE     Delete a file or set of files\r\n");
+    rpm_puts("RMDIR           Remove a directory\r\n");
     rpm_puts("TIME            Set or show the current system time\r\n");
     rpm_puts("VER             Show the version of RP/M software\r\n");
+    rpm_puts("VOL             Show the volume label of a disk device\r\n");
     rpm_puts("EXIT            Close down the command interpreter\r\n");
     rpm_puts("\r\n");
     rpm_puts("Enter COMMAND -h for further information on any of the above commands.\r\n");
