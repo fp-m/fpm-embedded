687a3657302ef2c764528f994f83c2fc943cc953
Serge Vakulenko <serge.vakulenko@gmail.com>
2023-02-25 01:30:51 -0800

    Start implementing vol.

--- kernel/cmd/cmd_vol.c
+++ kernel/cmd/cmd_vol.c
@@ -4,19 +4,46 @@
 #include <rpm/api.h>
 #include <rpm/getopt.h>
 #include <rpm/internal.h>
+#include <rpm/fs.h>
+#include <rpm/diskio.h>
+
+static void print_volume_info(const char *path)
+{
+    //TODO
+    rpm_printf("      Disk Drive: sd:\r\n");
+    rpm_printf("      Disk Model: Samsung SDHC\r\n");
+    rpm_printf("       Disk Size: 40.0 Mbytes\r\n");
+    rpm_printf("   Unique Number: 12345678\r\n");
+    rpm_printf("Filesystem Label: mydisk\r\n");
+    rpm_printf("   Serial Number: aabbccdd\r\n");
+}
+
+static void set_label(const char *label)
+{
+    fs_result_t result = f_setlabel(label);
+    if (result != FR_OK) {
+        rpm_puts(f_strerror(result));
+        rpm_puts("\r\n\n");
+    }
+}
 
 void rpm_cmd_vol(int argc, char *argv[])
 {
     static const struct rpm_option long_opts[] = {
         { "help", RPM_NO_ARG, NULL, 'h' },
+        { "label", RPM_REQUIRED_ARG, NULL, 'l' },
         {},
     };
     struct rpm_opt opt = {};
 
-    while (rpm_getopt(argc, argv, "h", long_opts, &opt) >= 0) {
+    while (rpm_getopt(argc, argv, "hl:", long_opts, &opt) >= 0) {
         switch (opt.ret) {
         case 1:
-            rpm_printf("%s: Unexpected argument `%s`\r\n\n", argv[0], opt.arg);
+            print_volume_info(opt.arg);
+            return;
+
+        case 's':
+            set_label(opt.arg);
             return;
 
         case '?':
@@ -27,10 +54,14 @@ void rpm_cmd_vol(int argc, char *argv[])
         case 'h':
             rpm_puts("Usage:\r\n"
                      "    vol [sd: | flash:]\r\n"
+                     "    vol -l disk:label\r\n"
+                     "Options:\r\n"
+                     "    -l, --label   Set the filesystem label\r\n"
                      "\n");
             return;
         }
     }
 
-    rpm_puts("Not implemented yet.\r\n\n");
+    const char *drive = disk_name[f_drive()];
+    print_volume_info(drive);
 }
