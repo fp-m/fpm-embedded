4a8c2bb420fca1c9e9cf876c8cd262660b587a53
Serge Vakulenko <serge.vakulenko@sima.ai>
2025-01-09 02:41:28 +0000

    In loader_test, avoid calling libc from mock_puts.

--- tests/loader_test.cpp
+++ tests/loader_test.cpp
@@ -5,6 +5,7 @@
 #include <rpm/api.h>
 #include <rpm/loader.h>
 #include <rpm/elf.h>
+#include <sys/syscall.h>
 
 TEST(loader, elf_binary)
 {
@@ -35,9 +36,22 @@ TEST(loader, linked_symbols)
 
 static void mock_puts(const char *message)
 {
+#if __ARM_ARCH_ISA_A64
+    // Avoid calling libc here, otherwise the test may fail on arm64 machine.
+    const char *end = message;
+    while (*end)
+        end++;
+
+    register int x0 __asm__("x0") = 1; /* stdout */
+    register const char *x1 __asm__("x1") = message;
+    register size_t x2 __asm__("x2") = end - message;
+    register int x8 __asm__("x8") = SYS_write;
+    asm volatile("svc #0" : : "r" (x0), "r" (x1), "r" (x2), "r" (x8) : "memory");
+#else
     fputs(message, stdout);
     fflush(stdout);
     // TODO: save output.
+#endif
 }
 
 TEST(loader, run_puts)
