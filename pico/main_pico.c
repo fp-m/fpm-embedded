ab3c56cc2ea7e3386ec97d91035488ad46e4b9c0
Serge Vakulenko <serge.vakulenko@gmail.com>
2025-01-27 21:48:13 -0800

    Use pico_aon_timer.

--- pico/main_pico.c
+++ pico/main_pico.c
@@ -5,30 +5,8 @@
 #include <fpm/diskio.h>
 #include <fpm/context.h>
 #include <pico/stdlib.h>
-#if HAS_RP2040_RTC
-#   include "hardware/rtc.h"
-#endif
 
-void setup_date_time()
-{
-#if HAS_RP2040_RTC
-    // Start the real time clock.
-    rtc_init();
-
-    //TODO: get time/date from battery backed RTC or from filesystem
-    datetime_t t = {
-        .year  = BUILD_YEAR,
-        .month = BUILD_MONTH,
-        .day   = BUILD_DAY,
-        .dotw  = BUILD_DOTW, // 0 is Sunday, so 5 is Friday
-        .hour  = BUILD_HOUR,
-        .min   = BUILD_MIN,
-        .sec   = BUILD_SEC,
-    };
-    rtc_set_datetime(&t);
-    sleep_us(64);
-#endif
-}
+extern void fpm_init_datetime(void);
 
 int main()
 {
@@ -40,7 +18,7 @@ int main()
     extern char end[], __HeapLimit[];
     fpm_heap_init(&context_base, (size_t)&end[0], __HeapLimit - end);
 
-    setup_date_time();
+    fpm_init_datetime();
     disk_setup();
 
     // Try to mount flash at startup.
--- pico/rtc_pico.c
+++ pico/rtc_pico.c
@@ -1,26 +1,22 @@
 #include <fpm/api.h>
 #include "pico/stdlib.h"
-#if HAS_RP2040_RTC
-#   include "hardware/rtc.h"
-#endif
+#include "pico/aon_timer.h"
 
 //
 // Get date and time (local).
 //
 void fpm_get_datetime(int *year, int *month, int *day, int *dotw, int *hour, int *min, int *sec)
 {
-#if HAS_RP2040_RTC
-    datetime_t now = {};
-    rtc_get_datetime(&now);
+    struct tm timestamp = {};
+    aon_timer_get_time_calendar(&timestamp);
 
-    *year = now.year;
-    *month = now.month;
-    *day = now.day;
-    *dotw = now.dotw;
-    *hour = now.hour;
-    *min = now.min;
-    *sec = now.sec;
-#endif
+    *year = 1900 + timestamp.tm_year;
+    *month = 1 + timestamp.tm_mon;
+    *day = timestamp.tm_mday;
+    *dotw = timestamp.tm_wday;
+    *hour = timestamp.tm_hour;
+    *min = timestamp.tm_min;
+    *sec = timestamp.tm_sec;
 }
 
 //
@@ -28,18 +24,32 @@ void fpm_get_datetime(int *year, int *month, int *day, int *dotw, int *hour, int
 //
 void fpm_set_datetime(int year, int month, int day, int hour, int min, int sec)
 {
-#if HAS_RP2040_RTC
-    datetime_t now = {};
-
-    now.year = year;
-    now.month = month;
-    now.day = day;
-    now.dotw = fpm_get_dotw(year, month, day);
-    now.hour = hour;
-    now.min = min;
-    now.sec = sec;
-
-    rtc_set_datetime(&now);
+    const struct tm timestamp = {
+        .tm_sec  = sec,
+        .tm_min  = min,
+        .tm_hour = hour,
+        .tm_mday = day,
+        .tm_mon  = month - 1,
+        .tm_year = year - 1900,
+        .tm_wday = fpm_get_dotw(year, month, day),
+    };
+    aon_timer_set_time_calendar(&timestamp);
     sleep_us(64);
-#endif
+}
+
+void fpm_init_datetime()
+{
+    //TODO: get time/date from battery backed RTC or from filesystem
+    static const struct tm build_timestamp = {
+        .tm_sec  = BUILD_SEC,
+        .tm_min  = BUILD_MIN,
+        .tm_hour = BUILD_HOUR,
+        .tm_mday = BUILD_DAY,
+        .tm_mon  = BUILD_MONTH - 1,
+        .tm_year = BUILD_YEAR - 1900,
+        .tm_wday = BUILD_DOTW, // 0 is Sunday, so 5 is Friday
+    };
+
+    aon_timer_start_calendar(&build_timestamp);
+    sleep_us(64);
 }
